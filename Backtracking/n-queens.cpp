e39e5bbf155d698d663500905eee3f0230fc88c6
Snehil Gupta <snehilgupta92@gmail.com>
2021-05-24 19:27:29 +0530

    Comments

--- Backtracking/n-queens.cpp
+++ Backtracking/n-queens.cpp
@@ -1,19 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isSafe(int** board, int n, int p, int q) {
-    
+bool isSafe(int **board, int n, int p, int q)
+{
+
     // Check for rows in the column above the current row
-    for(int i = 0; i < p; i++) {
-        if(board[i][q] == 1)
+    for (int i = 0; i < p; i++)
+    {
+        if (board[i][q] == 1)
             return false;
     }
 
     int x = p, y = q;
 
     // Chcek for right upper diagonal
-    while(x >= 0 && y < n) {
-        if(board[x][y] == 1)
+    while (x >= 0 && y < n)
+    {
+        if (board[x][y] == 1)
             return false;
         x--;
         y++;
@@ -21,8 +24,9 @@ bool isSafe(int** board, int n, int p, int q) {
 
     // Check for left upper diagonal
     x = p, y = q;
-    while(x >= 0 && y >= 0) {
-        if(board[x][y] == 1)
+    while (x >= 0 && y >= 0)
+    {
+        if (board[x][y] == 1)
             return false;
         x--;
         y--;
@@ -31,22 +35,24 @@ bool isSafe(int** board, int n, int p, int q) {
     return true;
 }
 
-
-bool nqueens(int** board, int x, int n) {
+bool nqueens(int **board, int x, int n)
+{
     // check if all queens have been placed
-    if(x >= n)
+    if (x >= n)
         return true;
-    
+
     // if not, continue with the given row
-    for(int i = 0; i < n; i++) {
-        // iff it is safe to place the queen in row x and column i
-        if(isSafe(board, n, x, i)) {
+    for (int i = 0; i < n; i++)
+    {
+        // if it is safe to place the queen in row x and column i
+        if (isSafe(board, n, x, i))
+        {
             board[x][i] = 1;
-            
+
             // if this arrangement of queens results in solution, continue
-            if(nqueens(board, x+1, n))
+            if (nqueens(board, x + 1, n))
                 return true;
-            
+
             // else, backtrack to last decision point
             board[x][i] = 0;
         }
@@ -55,33 +61,43 @@ bool nqueens(int** board, int x, int n) {
     return false;
 }
 
-int main() {
-    cout<<"Enter number of queens: ";
-    int n; cin>>n;
-    int** board = new int* [n];
-    for(int i = 0; i < n; i++) {
+int main()
+{
+    cout << "Enter number of queens: ";
+    int n;
+    cin >> n;
+    int **board = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
         board[i] = new int[n];
     }
 
     // 2D matrix initialisation
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             board[i][j] = 0;
         }
-    }    
+    }
 
     // if queens can be placed successfully
-    if(nqueens(board, 0, n)) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                cout<<board[i][j]<<" ";
+    if (nqueens(board, 0, n))
+    {
+        cout << "Arrangement of Queens is as follows:\n";
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << board[i][j] << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
     // not possible to place the queens
-    else {
-        cout<<"No possible arrangement of "<<n<<"Queens found!";
+    else
+    {
+        cout << "No possible arrangement of " << n << "Queens found!";
     }
 
     return 0;
--- Backtracking/sumOfSubset.cpp
+++ Backtracking/sumOfSubset.cpp
@@ -1,59 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool subsetSum(int a[], int n, int val, vector<int> &v, int sum, int total) {
-    if(sum == val) {
-        return true;    
-    }
-    
-    else if(n < 0) {
-        return false;
-    }
-
-    else if(sum + total < val || sum > val) {
-        return false;
-    }
-
-    v.push_back(a[n]);
-    if(!subsetSum(a, n-1, val, v, sum+a[n], total-a[n])) {
-        v.pop_back();
-        if(subsetSum(a, n-1, val, v, sum, total-a[n]))
-            return true;
-    }
-    else {
+bool subsetSum(int a[], int n, int val, vector<int> &v, int sum, int total)
+{
+    // if value has been generated using a subset, return true
+    if (sum == val)
+    {
         return true;
     }
+
+    // if no elements left in array, return false
+    else if (n < 0)
+    {
+        return false;
+    }
+
+    // if sum has exceeded the value or if value is greater than summation of all elements
+    else if (sum + total < val || sum > val)
+    {
+        return false;
+    }
+
+    // push the current element in solution vector
+    v.push_back(a[n]);
+    if (!subsetSum(a, n - 1, val, v, sum + a[n], total - a[n]))
+    {
+        // if that branch does not give the solution, backtrack and choose the next element
+        v.pop_back();
+
+        //call the function again without considering the current element
+        if (subsetSum(a, n - 1, val, v, sum, total - a[n]))
+            // return true, if this branch gives the solution
+            return true;
+    }
+    else
+    {
+        return true;
+    }
+    // return false in all other cases
     return false;
 }
 
-int main() {
-    int n; cin>>n;
+int main()
+{
+    cout << "Enter number of elements: ";
+    int n;
+    cin >> n;
     int a[n], total = 0;
-    for(int i = 0; i < n; i++) {
-        cin>>a[i];
+    cout << "Enter elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
         total += a[i];
     }
 
-    int val; cin>>val;
-    if(val == 0) {
-        cout<<"Empty subset can generate 0";
+    cout << "Enter value to be generated using subset: ";
+    int val;
+    cin >> val;
+    if (val == 0) //if value to be formed is 0, empty subset
+    {
+        cout << "Empty subset can generate 0";
     }
-    else if(n == 0) {
-        cout<<"cannot generate any possible sum.";
+    else if (n == 0) //if there are no elements in array, value cannot be generated
+    {
+        cout << "cannot generate any possible sum.";
     }
-    else {
+    else
+    {
         vector<int> v;
-        if(subsetSum(a, n-1, val, v, 0, total)) {
-            cout<<"Subset Sum generation is possible!\nSubset includes: ";
-            for(auto itr : v) {
-                cout<<itr<<" ";
+        if (subsetSum(a, n - 1, val, v, 0, total)) //check for subset possibility
+        {
+            cout << "Subset Sum generation is possible!\nSubset includes: ";
+            reverse(v.begin(), v.end());
+            for (auto itr : v)
+            {
+                cout << itr << " ";
             }
         }
-        else {
-            cout<<"cannot generate sum using any subset.\n";
-            for(auto itr : v) {
-                cout<<itr<<" ";
-            }
+        else
+        {
+            cout << "cannot generate sum using any subset.\n";
+            // for (auto itr : v)
+            // {
+            //     cout << itr << " ";
+            // }
         }
     }
     return 0;
