7cdbc985b7d73ba98366a47b4807ac4ed94886e7
Snehil Gupta <snehilgupta92@gmail.com>
2021-06-05 18:26:49 +0530

    Generate all subsets of given set of numbers

---
+++ Bit_Manipulation/subsetGen.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void subset(int *a, int n)
+{
+    for (int i = 0; i < (1 << n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+                cout << a[j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int *a = new int[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    subset(a, n);
+}
