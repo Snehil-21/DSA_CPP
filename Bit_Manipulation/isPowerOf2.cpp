8255304d46e97cea08e367480061d911e6ef05f5
Snehil Gupta <snehilgupta92@gmail.com>
2021-06-05 18:09:48 +0530

    Power of 2

---
+++ Bit_Manipulation/isPowerOf2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter number: ";
+    int x;
+    cin >> x;
+    if ((x & (x - 1)) == 0)
+        cout << x << " is a power of 2" << endl;
+    else
+        cout << x << " is not a power of 2" << endl;
+}
