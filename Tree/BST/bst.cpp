20af4f33f11d2982e639874c7efac9bc3196fd0a
Snehil Gupta <snehilgupta92@gmail.com>
2021-08-14 23:51:15 +0530

    Update

--- Tree/BST/bst.cpp
+++ Tree/BST/bst.cpp
@@ -61,6 +61,62 @@ public:
         cout << val << " not found in the tree.";
     }
 
+    // FIND SUCCESSOR OF NODE IN TREE
+    Node *minSuccessor(Node *root)
+    {
+        while (root && root->left)
+        {
+            root = root->left;
+        }
+        return root;
+    }
+
+    // DELETE A NODE
+    Node *deleteNode(int val, Node *root)
+    {
+        if (!root)
+        {
+            return NULL;
+        }
+        if (root->data == val)
+        {
+            if (!root->left && !root->right)
+            {
+                delete (root);
+                return NULL;
+            }
+            else if (root->left && !root->right)
+            {
+                root->data = root->left->data;
+                root->left = NULL;
+                delete (root->left);
+                return root;
+            }
+            else if (!root->left && root->right)
+            {
+                root->data = root->right->data;
+                root->right = NULL;
+                delete (root->right);
+                return root;
+            }
+            else
+            {
+                Node *x = minSuccessor(root->right);
+                root->data = x->data;
+                root->right = deleteNode(x->data, root->right);
+            }
+        }
+        else if (root->data > val)
+        {
+            root->left = deleteNode(val, root->left);
+        }
+        else if (root->data < val)
+        {
+            root->right = deleteNode(val, root->right);
+        }
+        return root;
+    }
+
     // TRAVERSE THE TREE IN INORDER
 
     void inorder(Node *root)
@@ -77,12 +133,18 @@ int main()
 {
     BST b;
     Node *root = NULL;
-    root = b.insert(5, root);
+    root = b.insert(10, root);
+    b.insert(5, root);
+    b.insert(15, root);
     b.insert(4, root);
-    b.insert(3, root);
     b.insert(6, root);
+    b.insert(14, root);
+    b.insert(16, root);
     b.inorder(root);
     cout << endl;
+    b.deleteNode(15, root);
+    cout << endl;
+    b.inorder(root);
     // b.search(4, root);
     return 0;
 }
